789e8f4ba961fd26ac4c0d5d041815858eac1450
jonz94 <jody16888@gmail.com>
2020-11-21 04:15:49 +0800

    👍 build(deps): bump `@angular` & `@angular-devkit` packages from 10.1.4 to 11.0.2

--- src/app/video-downloader/video-downloader.page.spec.ts
+++ src/app/video-downloader/video-downloader.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { VideoDownloaderPage } from './video-downloader.page';
 
@@ -6,16 +6,18 @@ describe('VideoDownloaderPage', () => {
   let component: VideoDownloaderPage;
   let fixture: ComponentFixture<VideoDownloaderPage>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [VideoDownloaderPage],
-      imports: [IonicModule.forRoot()],
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [VideoDownloaderPage],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(VideoDownloaderPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(VideoDownloaderPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }),
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
